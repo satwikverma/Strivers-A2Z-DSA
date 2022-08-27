a5c2fd5126a3861392b3d98be92b24f4162d6351
satwikverma <satwikverma18@gmail.com>
2022-08-27 22:16:27 +0530

    Commit 2

---
+++ Step 1 - Learn the Basics/Step 1.1 - Things to know in Java/2. Data Types/Primitives.java
@@ -0,0 +1,22 @@
+class Primitives {
+    public static void main(String[] args) {
+        boolean isValid = true;
+        System.out.println("Boolean value isValid = " + isValid);
+
+        byte marks = 99;
+        System.out.println("Byte value marks = " + marks);
+
+        float pi = 3.14f;
+        System.out.println("Float value pi = " + pi);
+
+        float div = 15.0f / 4.0f;
+        System.out.println("Float value div = " + div);
+
+        long views = 1000000000;
+        System.out.println("Long value views = " + views);
+
+        char gender = 'M';
+        System.out.println("Char value gender = " + gender);
+
+    }
+}
