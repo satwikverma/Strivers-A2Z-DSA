b0021f97cb4af5f17ceedced1931e07b58b7c7c0
satwikverma <satwikverma18@gmail.com>
2022-08-28 09:38:52 +0530

    Commit 2

--- Step 1 - Learn the Basics/Step 1.1 - Things to know in Java/2. Data Types/Ex1.java
+++ Step 1 - Learn the Basics/Step 1.1 - Things to know in Java/2. Data Types/Ex1.java
@@ -8,9 +8,12 @@ public class Point {
 
 public class Ex1 {
     public static void main(String[] args) {
-        Point p = new Point();
-        p.x = 10;
-        p.y = 20;
-        System.out.println(p.x + " " + p.y);
+        Point p1 = new Point();
+        p1.x = 10;
+        p1.y = 20;
+
+        Point p2 = p1;
+        p2.x = 30;
+        System.out.println(p1.x + " " + p2.x);
     }
 };
