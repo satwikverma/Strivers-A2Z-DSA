e3b59950401e7b575fc7d7903e66d6da120a8a21
satwikverma <satwikverma18@gmail.com>
2022-09-06 10:18:28 +0530

    Commit 4

---
+++ Step 1 - Learn the Basics/Step 1.1 - Things to know in Java/7. while loops/while_loop_syntax.java
@@ -0,0 +1,12 @@
+// while loop syntax
+while(condition) {
+    // code
+    // update condtion
+}
+
+// Example - Print numbers 10 to 1
+int T = 10;
+    while(T > 0) {
+        System.out.println(T);
+        T-- ;
+    }
